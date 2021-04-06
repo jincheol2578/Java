81161dbba34a905984247e4c6fc300d2904b14f5
jincheol2578 <jincheol2578@gmail.com>
2021-04-06 10:16:46 +0900

    21.04.06

--- chap01/src/sec06/ch05/MethodMission10.java
+++ chap01/src/sec06/ch05/MethodMission10.java
@@ -5,38 +5,30 @@ import java.util.Scanner;
 public class MethodMission10 {
 
 	public static void main(String[] args) {
-
+		Scanner scan = new Scanner(System.in);
 		String[] menuNmArr = { "콜라", "사이다", "환타", "스프라이트", "망고주스", "포도주스" };
 		int[] menuPriceArr = { 1000, 1200, 1300, 1500, 2000, 3000 };
 
 		printMenu(menuNmArr, menuPriceArr);
+		int selectedNum = scan.nextInt();
+		System.out.println("끝");
 	}
 
-	public static void printMenu(String[] menuNmArr, int[] menuPriceArr) {
-		Scanner scan = new Scanner(System.in);
-		System.out.println("<메뉴>");
-		System.out.println("0. 종료");
-		for (int i = 0; i < menuNmArr.length; i++) {
-			System.out.printf("%d. %s (%d원)\n", i + 1, menuNmArr[i], menuPriceArr[i]);
-		}
-		while (true) {
-			System.out.print("선택 > ");
-			int scanVal = 0;
-			scanVal = scan.nextInt();
-			if (scanVal == 0) {
-				System.out.println("종료");
-				break;
-			} else if (scanVal <= menuNmArr.length) {
-				System.out.printf("%s를 선택하셨습니다. (%d원)\n", menuNmArr[scanVal], menuPriceArr[scanVal]);
-			} else {
-				System.out.println("없는 상품입니다.");
-				continue;
-			}
-		}
+	public static boolean chooseMenu(Scanner scan, int len) {
+		System.out.println("선택 > ");
+		int seletedNum = scan.nextInt();
+		return true;
 	}
-	
-	public static void printMenu2(String[] menuNmArr, int[] menuPriceArr) {
+	public static void printMenu(String[] mnArr, int[] priceArr) {
 		
+	}
+
+	public static void printMenu2(String[] mnArr, int[] priceArr) {
+		String str = "<메뉴>\n";
+		str += "종료\n";
+		for (int i = 0; i < mnArr.length; i++) {
+			str += String.format("%d. %s (%,d원)\n", i + 1, mnArr[i], priceArr[i]);
+		}
 	}
 
 }
---
+++ chap01/src/sec06/ch06/InstanceExam.java
@@ -0,0 +1,15 @@
+package sec06.ch06;
+
+public class InstanceExam {
+	public static void main(String[] args) {
+		
+	}
+}
+class Dog{
+	String name;
+	int age;
+	
+	void bark() {
+		System.out.println("멍멍");
+	}
+}
