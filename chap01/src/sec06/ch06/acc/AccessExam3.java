d5b96a3a2137a272937308fb8a1d431f5cecb234
jincheol2578 <jincheol2578@gmail.com>
2021-04-13 16:46:55 +0900

    21.04.13

--- chap01/src/sec06/ch06/acc/AccessExam3.java
+++ chap01/src/sec06/ch06/acc/AccessExam3.java
@@ -1,8 +1,11 @@
 package sec06.ch06.acc;
 
+import sec06.ch07.RemoteControl;
+
 public class AccessExam3 {
 	public static void main(String[] args) {
 //		Access acc = new Access();  접근X  package가 다름 Access는 defaut
+		System.out.println(RemoteControl.AGE);
 		
 	}
 }
